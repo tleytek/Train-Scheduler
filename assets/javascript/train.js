280e150fe827e7ad40dc8d04ec6a72b6be3e9def
tleytek <lucidmat94@gmail.com>
2018-07-26 14:12:22 -0400

    Finished moment.js

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -39,13 +39,6 @@ $("#add-train-btn").on("click", function(event) {
   //Uploads train data to the database
   database.ref().push(newTrain);
 
-  // Logs everything to console
-  console.log(newTrain.name);
-  console.log(newTrain.destination);
-  console.log(newTrain.firstTrain);
-  console.log(newTrain.frequency);
-  console.log(trainFrequency);
-
   alert("Train successfully added");
 
   // Clears all of the text-boxes
@@ -57,26 +50,43 @@ $("#add-train-btn").on("click", function(event) {
 
 // 3. Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot) {
-  console.log(childSnapshot.val());
-
   //Store everything into a variable.
   var trainName = childSnapshot.val().name;
   var trainDestination = childSnapshot.val().destination;
   var firstTrain = childSnapshot.val().firstTrain;
-  var trainFrequency = childSnapshot.val().trainFrequency;
+  var trainFrequency = childSnapshot.val().frequency;
 
-  // Employee Info
-  console.log(trainName);
-  console.log(trainDestination);
-  console.log(firstTrain);
-  console.log(trainFrequency);
+  var firstTrainTimeConverted = moment(firstTrain, "HH:mm").subtract(
+    1,
+    "years"
+  );
+  var diffTime = moment().diff(moment(firstTrainTimeConverted), "minutes");
+  var tRemainder = diffTime % trainFrequency;
+  var tMinutesAway = trainFrequency - tRemainder;
+  var nextArrival = moment()
+    .add(tMinutesAway, "minutes")
+    .format("HH:mm");
 
   var newRow = $("<tr>").append(
     $("<td>").text(trainName),
     $("<td>").text(trainDestination),
-    $("<td>").text(firstTrain),
-    $("<td>").text(trainFrequency)
+    $("<td>").text(trainFrequency),
+    $("<td>").text(nextArrival),
+    $("<td>").text(tMinutesAway)
   );
 
   $("#train-table > tbody").append(newRow);
+  //Original Idea:
+  //get the first train time and then add the frequency(min) to first train time to get next arrival.
+  // subtract next arrival time by current time to get minutes away.
+  // how do we create a new 'next arrival' once the 'next arrival' time is met the first time.
+
+  //The right way for time/moment:
+  //take the users inputted time and subtract a year from it so that our time
+  //is always ahead of the users, otherwise we will get negative time.
+  //grab the difference in minutes between the user inputted time that was pushed back a year behind and the current time.
+  //Modulus the difference in time (total minutes from user inputted time and current time) by the frequency of train time.
+  //subtract the time frequency of trains by the modulus remainder of the current time and user
+  //time to get the time remaining until the next train
+  //next train arrival time is calculated by adding tMinutesAway to the current time.
 });
